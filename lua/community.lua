cf95442b5dc38ade36f2eb2f44299a8010016387
donkanaille <blutigerwilliam@mail.de>
2024-12-12 18:30:05 +0100

    changes

--- lua/community.lua
+++ lua/community.lua
@@ -1,4 +1,4 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 -- AstroCommunity: import any community modules here
 -- We import this file in `lazy_setup.lua` before the `plugins/` folder.
@@ -7,6 +7,117 @@ if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 ---@type LazySpec
 return {
   "AstroNvim/astrocommunity",
-  { import = "astrocommunity.pack.lua" },
+  -- { import = "astrocommunity.pack.lua" },
   -- import/override with your plugins folder
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  {
+    import = "astrocommunity.colorscheme.mellifluous-nvim",
+    opts = {
+       colorset = "mellifluous",
+       plugins = {
+        cmp = true,
+        blink_cmp = true,
+        indent_blankline = true,
+        nvim_tree = {
+            enabled = true,
+            show_root = false,
+        },
+        neo_tree = {
+            enabled = true,
+        },
+        telescope = {
+            enabled = true,
+            nvchad_like = true,
+        },
+        startify = true,
+        gitsigns = true,
+        neorg = true,
+        nvim_notify = true,
+        aerial = true,
+        neotest = true,
+        lazy = true,
+        mason = true,
+    },
+    dim_inactive = false,
+    styles = { -- see :h attr-list for options. set {} for NONE, { option = true } for option
+        main_keywords = {},
+        other_keywords = {},
+        types = {},
+        operators = {},
+        strings = {},
+        borders = true,
+        functions = {},
+        constants = {},
+        comments = { italic = true },
+        markup = {
+            headings = { bold = true },
+        },
+        folds = {},
+    },
+    transparent_background = {
+        enabled = false,
+        lightness = function(bg) -- used for bg shades
+            -- This method tries to keep brighter colorsets bright and
+            -- dimmer colorsets dim and still lighten the shades up so that
+            -- the colorsets have more chance to look good with transparent
+            -- background on brighter wallpapers.
+            local old_lightness = bg:get_hsl().l
+            local threshold = 20
+            local baseline = 10
+            if old_lightness < threshold then
+                -- We will assume that the dimmest of transparent
+                -- background over users wallpaper is at least of baseline
+                -- lightness. Presuming old range is [0, threshold], let's
+                -- position the lightness relatively in a new range of
+                -- [baseline, threshold].
+                local position = old_lightness / threshold
+                local new_lightness = baseline + ((threshold - baseline) * position)
+                return new_lightness
+            end
+        end,
+        floating_windows = false,
+        telescope = false,
+        file_tree = false,
+        cursor_line = false,
+        status_line = false,
+    },
+    flat_background = {
+        line_numbers = true,
+        floating_windows = true,
+        file_tree = true,
+        cursor_line_number = true,
+    },
+  },
+  }, 
+     {
+    import = "astrocommunity.note-taking/neorg",
+    opts = {
+      load = {
+        ["core.defaults"] = {},  -- Loads default behaviour
+        ["core.concealer"] = {}, -- Adds pretty icons to your documents
+        ["core.keybinds"] = {},  -- Adds default keybindings
+        ["core.completion"] = {
+          config = {
+            engine = "nvim-cmp",
+          },
+        }, -- Enables support for completion plugins
+        ["core.journal"] = {
+          config = {
+            journal_folder = "./journal",
+          },
+        },                  -- Enables support for the journal module
+        ["core.dirman"] = { -- Manages Neorg workspaces
+          config = {
+            workspaces = {
+              notizen = "~/OneDrive/notizen",
+            },
+            autochdir = true,     -- Automatically change the directory to the current workspace's root every time
+
+            index = "index.norg", -- The name of the main (root) .norg file
+            default_workspace = "notizen",
+          },
+        },
+      },
+    },
+  },
 }
--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -1,4 +1,4 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+--if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 -- You can also add or configure plugins by creating files in this `plugins/` folder
 -- Here are some examples:
@@ -23,30 +23,59 @@ return {
     opts = function(_, opts)
       -- customize the dashboard header
       opts.section.header.val = {
-        " █████  ███████ ████████ ██████   ██████",
-        "██   ██ ██         ██    ██   ██ ██    ██",
-        "███████ ███████    ██    ██████  ██    ██",
-        "██   ██      ██    ██    ██   ██ ██    ██",
-        "██   ██ ███████    ██    ██   ██  ██████",
-        " ",
-        "    ███    ██ ██    ██ ██ ███    ███",
-        "    ████   ██ ██    ██ ██ ████  ████",
-        "    ██ ██  ██ ██    ██ ██ ██ ████ ██",
-        "    ██  ██ ██  ██  ██  ██ ██  ██  ██",
-        "    ██   ████   ████   ██ ██      ██",
-      }
+	[[ _______             ____   ____.__         ]],
+	[[ \      \   ____  ___\   \ /   /|__| _____  ]],
+	[[ /   |   \_/ __ \/  _ \   Y   / |  |/     \ ]],
+	[[/    |    \  ___(  <_> )     /  |  |  Y Y  \]],
+	[[\____|__  /\___  >____/ \___/   |__|__|_|  /]],
+	[[        \/     \/                        \/ ]],      }
       return opts
     end,
   },
 
-  -- You can disable default plugins as follows:
-  { "max397574/better-escape.nvim", enabled = false },
+  {
+    "goolord/alpha-nvim",
+    opts = function(_, opts) -- override the options using lazy.nvim
+      local get_icon = require("astroui").get_icon
+      
+      opts.section.buttons.val = {
+
+        opts.button("LDR n  ", get_icon("FileNew", 2, true) .. "New File  "),
+        opts.button("LDR f f", get_icon("Search", 2, true) .. "Find File  "),
+        opts.button("LDR f o", get_icon("DefaultFile", 2, true) .. "Recents  "),
+        opts.button("LDR f w", get_icon("WordFile", 2, true) .. "Find Word  "),
+        opts.button("LDR f '", get_icon("Bookmarks", 2, true) .. "Bookmarks  "),
+        opts.button("LDR S l", get_icon("Refresh", 2, true) .. "Last Session  "),
+        opts.button("w", "  Notizen", "<CMD>:cd /mnt/daten/OneDrive/notizen<CR>:edit /mnt/daten/OneDrive/notizen/index.norg<CR>"),
+        opts.button("q", "󰅚  Quit", "<CMD>quit<CR>"),
+      }
+
+    opts.config.layout = {
+      { type = "padding", val = vim.fn.max { 2, vim.fn.floor(vim.fn.winheight(0) * 0.2) } },
+      opts.section.header,
+      { type = "padding", val = 2 },
+      opts.section.buttons,
+      { type = "padding", val = 3 },
+      opts.section.footer,
+    }
+
+    opts.section.buttons.opts.spacing = 0
+    opts.section.header.opts.hl = "Error"
+    opts.section.footer.opts.hl = "Comment"
+--    opts.button.opts.hl_shortcut = "Keyword"
+--    opts.button.opts.hl=""
+
+    end,
+  },
+
+  -- You can disable/enable default plugins as follows:
+  { "max397574/better-escape.nvim", enabled = true },
 
   -- You can also easily customize additional setup of plugins that is outside of the plugin's setup call
   {
     "L3MON4D3/LuaSnip",
     config = function(plugin, opts)
-      require "astronvim.plugins.configs.luasnip"(plugin, opts) -- include the default astronvim config that calls the setup call
+      require "astronvim.plugins.configs.luasnip" (plugin, opts) -- include the default astronvim config that calls the setup call
       -- add more custom luasnip configuration such as filetype extend or custom snippets
       local luasnip = require "luasnip"
       luasnip.filetype_extend("javascript", { "javascriptreact" })
@@ -56,7 +85,7 @@ return {
   {
     "windwp/nvim-autopairs",
     config = function(plugin, opts)
-      require "astronvim.plugins.configs.nvim-autopairs"(plugin, opts) -- include the default astronvim config that calls the setup call
+      require "astronvim.plugins.configs.nvim-autopairs" (plugin, opts) -- include the default astronvim config that calls the setup call
       -- add more custom autopairs configuration such as custom rules
       local npairs = require "nvim-autopairs"
       local Rule = require "nvim-autopairs.rule"
@@ -64,18 +93,18 @@ return {
       npairs.add_rules(
         {
           Rule("$", "$", { "tex", "latex" })
-            -- don't add a pair if the next character is %
-            :with_pair(cond.not_after_regex "%%")
-            -- don't add a pair if  the previous character is xxx
-            :with_pair(
-              cond.not_before_regex("xxx", 3)
-            )
-            -- don't move right when repeat character
-            :with_move(cond.none())
-            -- don't delete if the next character is xx
-            :with_del(cond.not_after_regex "xx")
-            -- disable adding a newline when you press <cr>
-            :with_cr(cond.none()),
+          -- don't add a pair if the next character is %
+              :with_pair(cond.not_after_regex "%%")
+          -- don't add a pair if  the previous character is xxx
+              :with_pair(
+                cond.not_before_regex("xxx", 3)
+              )
+          -- don't move right when repeat character
+              :with_move(cond.none())
+          -- don't delete if the next character is xx
+              :with_del(cond.not_after_regex "xx")
+          -- disable adding a newline when you press <cr>
+              :with_cr(cond.none()),
         },
         -- disable for .vim files, but it work for another filetypes
         Rule("a", "a", "-vim")
